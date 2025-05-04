7229ca163aaac8e52c11f1bf6432c1bc1cab42f3
ccanunez <ccarvajal.nunez@gmail.com>
2025-05-04 03:32:06 -0400

    Update Setting up Plummer Sphere.py

--- Setting up Plummer Sphere.py
+++ Setting up Plummer Sphere.py
@@ -1,3 +1,14 @@
+###########################################################################
+###########################################################################
+###########################################################################
+'''
+Part 1.1.1
+Setting up a Plummer Sphere
+'''
+###########################################################################
+###########################################################################
+###########################################################################
+
 import matplotlib.pyplot as plt
 n = 17
 plt.rcParams.update({
@@ -88,4 +99,125 @@ for ax in axs:
     ax.set_ylabel('Frequency')
 
 plt.tight_layout()
+plt.show()
+
+###########################################################################
+###########################################################################
+###########################################################################
+'''
+Part 1.1.2
+Half total mass radius
+'''
+###########################################################################
+###########################################################################
+###########################################################################
+# Necessary initial condition
+r_pl = 1
+
+# Arrays to store values
+radii = []
+Mass = []
+
+# Iteration loop
+for j in range(0,n,1):
+    x1 = rng.random()
+    r = r_pl*((x1**(-2/3) - 1))**(-1/2)
+    Mass.append(x1)
+    radii.append(r)
+
+# Sorting the radii array
+r_half = np.sort(radii)
+
+'''Since there is only 1000 stars and each one
+with the same solar mass, the 500th position
+corresponds to the half mass radius'''
+print(f'the radius containing half the total mass is {r_half[500]:.2}')
+
+# Plotting
+fig, axs = plt.subplots(1, 2, figsize=(15, 4))
+
+axs[0].plot(radii)
+axs[0].set_title('original radii arrangement')
+
+axs[1].plot(r_half)
+axs[1].set_title('sorted radii arrangement')
+
+plt.show()
+
+# Plotting the mass
+plt.scatter(r_half, np.sort(Mass))
+plt.xlabel('sorted radii')
+plt.ylabel('sorted mass enclosed')
+plt.show()
+
+###########################################################################
+###########################################################################
+###########################################################################
+'''
+Part 1.2
+Velocity distribution
+'''
+###########################################################################
+###########################################################################
+###########################################################################
+from astropy import units as u
+from astropy import constants as const
+
+M_pl = 1e3  
+G = const.G.value
+
+# Arrays to store values
+V_esc = []
+V_tot = []
+v = []
+radius = []
+vx_values = []
+vy_values = []
+vz_values = []
+
+# Iterable loop
+k=0
+while k<=999:
+    x1 = rng.random()
+    x2 = rng.random()
+    if 0.1*x2 < x1**2 * (1 - x1**2)**(3.5):
+        vel = (2*G*M_pl/r_pl)**(0.5)*(1 + (radii[k]*2/r_pl**2))**(-1/4)
+        v_tot = x1*vel
+        
+        # Calculation of velocity components
+        vz = (1 - 2*x1)*v_tot
+        vx = (v_tot**2 - vz**2)**(0.5)*np.cos(2*np.pi*x2)
+        vy = (v_tot**2 - vz**2)**(0.5)*np.sin(2*np.pi*x2)
+        velocity = np.sqrt(vx**2 + vy**2 + vz**2)
+        
+        # Storing values
+        V_esc.append(vel)
+        V_tot.append(v_tot)
+        v.append(velocity)        
+        vx_values.append(vx)
+        vy_values.append(vy)
+        vz_values.append(vz)
+        radius.append(radii[k])       
+        
+        plt.scatter(x1,x2*0.1)
+        
+        k+=1
+
+# Plotting the function velocity distribution
+print(f'the total particles considered are {len(V_esc)} from the {n} total')
+plt.xlabel('random number x1')
+plt.ylabel('random number x2')
+plt.title('Accepted values')
+plt.hlines(y=0.1, xmin=0, xmax=1, colors='red', linestyles='dashed')
+plt.grid(alpha=0.3)
+plt.show()
+
+# Plotting the velocities over radius
+plt.scatter(radius, v, color='gray', label='particle´s velocity')
+plt.scatter(radius, V_esc, color='red', linewidths=0.1, label='scape velocities')
+plt.xlabel('radius [kpc]')
+plt.ylabel('velocity [km/s]')
+plt.xscale('log')
+plt.grid(alpha=0.3)
+plt.legend()
 plt.show()
