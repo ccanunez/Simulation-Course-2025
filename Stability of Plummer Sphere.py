5690c9131ebda2ff792e4ec9e4bacb690089a433
ccanunez <ccarvajal.nunez@gmail.com>
2025-05-09 16:14:02 -0400

    Update Stability of Plummer Sphere.py

--- Stability of Plummer Sphere.py
+++ Stability of Plummer Sphere.py
@@ -146,3 +146,67 @@ Plotting the Lagrangian radii
 ###########################################################################
 ###########################################################################
 ###########################################################################
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+# Initial parameters in G = 1 units
+G = 1
+Lsim = 1  # pc
+Msim = 1  # Msun
+Tsim = 14.9 * np.sqrt(Lsim**3 / Msim)
+Vsim = 980.4 * (Lsim / Tsim)
+
+# Files
+input_file = 'output.dat'
+output_file = 'data.csv'
+
+# Reads the input file in ASCII format
+df = pd.read_csv(input_file, delim_whitespace=True)
+
+# Transforms and saves the file in CSV format
+df.to_csv(output_file, index=False)
+print(f"Converted '{input_file}' to '{output_file}'")
+
+# Columns information extraction
+x_sim = df['x'].values
+y_sim = df['y'].values
+z_sim = df['z'].values
+part_id = df['id'].values
+snap = df['snap'].values
+
+# Compoute radius
+r = np.sqrt(x_sim**2 + y_sim**2 + z_sim**2)
+df['r'] = r  # Adds a new column for the radius
+
+# Obtains unique snaps and their first index
+snaps, ind_snaps = np.unique(snap, return_index=True)
+print(f'the first snapshot is {snaps[0]}')            # First snapshot
+print(f'the number of snapshots are {len(ind_snaps)}')      # Number of unique snapshots
+
+# Stacks radius per snapshor
+r_arr = []
+for s in tqdm(snaps, desc='Processing'):
+    pos = np.where(snap == s)[0]
+    r_arr.append(r[pos])
+
+# Compute percentiles
+r_half, r20, r80 = [], [], []
+for r_snapshot in r_arr:
+    r_sort = np.sort(r_snapshot)
+    N = len(r_sort)
+    r_half.append(r_sort[N // 2])
+    r20.append(r_sort[int(N * 0.2)])
+    r80.append(r_sort[int(N * 0.8)])
+
+# Plotting proccess of radius evolution
+plt.plot(snaps * Tsim, r20, c='navy', lw=1.5, label=r'$R_{20}$')
+plt.plot(snaps * Tsim, r_half, c='purple', lw=1.5, label=r'$R_{50}$')
+plt.plot(snaps * Tsim, r80, c='darkred', lw=1.5, label=r'$R_{80}$')
+plt.ylabel(r'$r\ [pc]$', fontsize=15)
+plt.xlabel(r'$t\ [Myr]$', fontsize=15)
+plt.ylim(0, 4)
+plt.legend()
+plt.show()
